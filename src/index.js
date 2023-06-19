3229fd33a233aee6da0a23c4f64f2da7d7d40796
BrianBeilby <brianbeilby98@gmail.com>
2023-06-18 22:03:53 -0700

    Started Youtube Search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+const { Client, IntentsBitField, EmbedBuilder, Embed } = require("discord.js");
+const Youtube = require("discord-youtube-api");
 
 const client = new Client({
   intents: [
@@ -9,6 +10,16 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ],
 });
+
+const youtube = new Youtube(process.env.GOOGLE_API_KEY);
+
+async function testAll() {
+  const video1 = await youtube.getVideo("https://www.youtube.com/watch?v=5NPBIwQyPWE");
+  const video2 = await youtube.getVideoByID("5NPBIwQyPWE");
+  const video3 = await youtube.searchVideos("big poppa biggie smalls");
+
+  console.log(video1, video2, video3);
+}
 
 client.on("ready", (c) => {
   console.log(`${c.user.tag} is online.`);
@@ -24,11 +35,41 @@ client.on("messageCreate", (msg) => {
   }
 });
 
-client.on('interactionCreate', (interaction) => {
+client.on("interactionCreate", (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'hey') {
-    interaction.reply('hey!');
+  if (interaction.commandName === "hey") {
+    interaction.reply("hey!");
+  }
+
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.get("first-number").value;
+    const num2 = interaction.options.get("second-number").value;
+
+    interaction.reply(`The sum is ${num1 + num2}`);
+  }
+
+  if (interaction.commandName === "embed") {
+    const embed = new EmbedBuilder()
+      .setTitle("Embed title")
+      .setDescription("This is an embed description")
+      .setColor("Random")
+      .addFields({
+        name: "Field title",
+        value: "Some random value",
+        inline: true,
+      },
+      {
+        name: "Second Field title",
+        value: "Some random value",
+        inline: true,
+      });
+
+    interaction.reply({ embeds: [embed] });
+  }
+
+  if (interaction.commandName === 'play') {
+    testAll();
   }
 });
 
