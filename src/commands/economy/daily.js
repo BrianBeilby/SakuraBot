1959b7741c1cd6c8de7db7cb0a4e3154b3d68582
BrianBeilby <brianbeilby98@gmail.com>
2023-06-27 14:27:36 -0700

    Added daily command

---
+++ src/commands/economy/daily.js
@@ -0,0 +1,61 @@
+const { Client, Interaction } = require("discord.js");
+const User = require("../../models/User");
+
+const dailyAmount = 1000;
+
+module.exports = {
+  name: "daily",
+  description: "Gives you your daily reward!",
+
+  /**
+   *
+   * @param {Client} client
+   * @param {Interaction} interaction
+   */
+  callback: async (client, interaction) => {
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: "You can only run this command inside a server.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    try {
+      await interaction.deferReply();
+
+      let query = {
+        userId: interaction.user.id,
+        guildId: interaction.guild.id,
+      };
+
+      let user = await User.findOne(query);
+
+      if (user) {
+        const lastDailyDate = user.lastDaily.toDateString();
+        const currentDate = new Date().toDateString();
+
+        if (lastDailyDate === currentDate) {
+          interaction.editReply(
+            "You have already claimed your daily reward today!"
+          );
+          return;
+        }
+      } else {
+        user = new User({
+          ...query,
+          lastDaily: new Date(),
+        });
+      }
+
+      user.balance += dailyAmount;
+      await user.save();
+
+      interaction.editReply(
+        `${dailyAmount} was added to your balance. Your new balance is ${user.balance}!`
+      );
+    } catch (error) {
+      console.log(`Error with /daily command: ${error}`);
+    }
+  },
+};
