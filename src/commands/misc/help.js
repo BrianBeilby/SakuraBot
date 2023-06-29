799188ece52049e5249a2022de145c2c1672c3d7
BrianBeilby <brianbeilby98@gmail.com>
2023-06-28 20:28:03 -0700

    Started help command

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -1,3 +1,6 @@
+const getLocalCommands = require("../../utils/getLocalCommands");
+const { Client, Interaction, EmbedBuilder } = require("discord.js");
+
 module.exports = {
     name: "help",
     description: "Provides information about the bot's commands!",
@@ -7,8 +10,36 @@ module.exports = {
     // options: Object[],
     // deleted: Boolean,
   
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
     callback: async (client, interaction) => {
+        const localCommands = getLocalCommands();
       
-    },
+        const fields = [];
+      
+        for (const command of localCommands) {
+          const { name, description, options, type, testOnly } = command;
+      
+          if (testOnly) continue;
+      
+          const field = {
+            name: type === "slash" ? `📜 Command: ${name}` : `📜 Context Menu Command: ${name}`,
+            value: `**Description:** ${description}\n**Options:** ${options ? options.map((option) => `\`${option.name}\``).join(", ") : "None"}`,
+            inline: false,
+          };
+      
+          fields.push(field);
+        }
+      
+        const embed = new EmbedBuilder()
+          .setTitle("📜 List of Commands")
+          .setColor("Green")
+          .addFields(fields);
+      
+        interaction.reply({ embeds: [embed], ephemeral: true });
+      },
   };
   
